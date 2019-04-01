484673eb038c510d7bebf56f6b850f22cf62e85a
huan670119408 <670119408@qq.com>
2019-04-01 23:47:45 +0800

    add

---
+++ pages/index/poster/poster.js
@@ -0,0 +1,14 @@
+Page({
+  data: {
+    title: "幽游白书",
+    images: {
+      large: "/image/game/game1"
+    },
+    rating: {
+      average: 8.6,
+      max: 10,
+      min: 0,
+      stars: "45"
+    }
+  }
+})
